a67ae19d9665f5f671d03d29ae5e2ef6e5e7e8f2
santi <santisevitz@hotmail.com>
2023-12-03 19:14:41 -0300

    arreglos de modificar tareaas

--- src/main/java/com/psa/soporte/repo/TareaRepo.java
+++ src/main/java/com/psa/soporte/repo/TareaRepo.java
@@ -5,7 +5,11 @@ import com.psa.soporte.modelos.Tarea;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TareaRepo extends CrudRepository<Tarea, Long> {
 
+
+    Optional<Tarea> findByTareaIdRemoto(Long tareaIdRemoto);
 }
